3edebceafa8e439600858fe947fd335f229d681f
Janusz G <400@poczta.fm>
2019-12-10 21:22:01 +0100

    Create aleatory.cpp

---
+++ Artificial Intelligence project 1/aleatory.cpp
@@ -0,0 +1,37 @@
+#include "aleatory.h"
+
+
+Aleatory::Aleatory()
+
+{
+
+//ctor
+
+}
+
+Aleatory::Aleatory(int boundMin, int bountMax,int seed)
+
+: seed_(seed),defGen_(seed_),randInt_(boundMin, bountMax)
+
+{
+
+/// Nothing here
+
+}
+
+int Aleatory::getRandInt()
+
+{
+
+return randInt_(defGen_);
+
+}
+
+
+Aleatory::~Aleatory()
+
+{
+
+//dtor
+
+}
